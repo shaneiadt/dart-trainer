80cec95242fb0cae8d2cea9a262f627db4aa1105
Shane O'Moore <shaneiadt@gmail.com>
2025-04-18 19:09:21 +0100

    refactor: component re-structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,13 @@
-import Darts from '../Darts/Darts'
 import Header from '../Header/Header'
 import Dartboard from '../Dartboard/Dartboard'
+import Footer from '../Footer/Footer'
 
 function App() {
   return (
     <>
       <Header />
-      <section className='sm:pt-20 md:pt-[100px]'>
-        <Dartboard />
-      </section>
-      <footer className='fixed w-full flex items-center justify-evenly content-center bottom-0'>
-        <div className='bg-green-950 flex px-[15px] py-[5px] rounded-[20px_20px_0px_0px]'>
-          <Darts />
-        </div>
-      </footer>
+      <Dartboard />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import Darts from "../Darts/Darts"
+
+const Footer = () => {
+    return (
+        <footer className='fixed w-full flex items-center justify-evenly content-center bottom-0'>
+            <div className='bg-green-950 flex px-[15px] py-[5px] rounded-[20px_20px_0px_0px]'>
+                <Darts />
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
